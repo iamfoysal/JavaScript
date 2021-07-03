ed12109f5e55057b9e61828380bc665322ed191b
foysal <foysalf652@gmail.com>
2021-07-04 00:56:24 +0600

    JavaScript-1 Library

---
+++ first_class/main.js
@@ -0,0 +1,8 @@
+/* start javaScript (document.write) library*/ 
+document.write("<br> Name: Mohammad Kawsar Alom Foysal <br> <hr>");
+document.write("Address: Feni, bangladesh<hr>");
+document.write('Phone: +88964575452<br>');
+/* start javaScript (alart) library*/ 
+alert("Wellcome!");
+/* start javaScript (console.log) library*/ 
+console.log('<br>This is Foysal <br>');
