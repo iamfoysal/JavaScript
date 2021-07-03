27e3a881f9365969ae65b7ece0acfd8d6af68e14
foysal <foysalf652@gmail.com>
2021-07-04 02:22:13 +0600

    variable

--- first_class/main.js
+++ first_class/main.js
@@ -1,8 +1,35 @@
 /* start javaScript (document.write) library*/ 
 document.write("<br> Name: Mohammad Kawsar Alom Foysal <br> <hr>");
 document.write("Address: Feni, bangladesh<hr>");
-document.write('Phone: +88964575452<br>');
+document.write('Phone: +88964575452<br> <hr>');
 /* start javaScript (alart) library*/ 
 alert("Wellcome!");
 /* start javaScript (console.log) library*/ 
 console.log('<br>This is Foysal <br>');
+
+
+
+
+/*======================================================================*/ 
+                   /* 2nd javaScript dataType*/
+ /*======================================================================*/ 
+
+ /* (typeof) dataType*/
+
+ typeof("i love JavaScript");  /*string type*/ 
+
+//  variable
+var firstMyName = "Mohammad Foysal <br>";
+document.write(firstMyName);
+firstMyName = " Mohammad Kawsar Alom Foysal";
+document.write(firstMyName);
+
+
+document.write("<br> name and age print: <br>")
+    var myNamr, age;
+
+    myName ="Robiul <br>";
+    age = 21 ;
+    document.write(myName);
+    document.write(age);
+
