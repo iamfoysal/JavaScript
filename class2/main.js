2d508e4ee8f26c5a1d3fe2c14b1cad4030f80c59
foysal <foysalf652@gmail.com>
2021-07-08 13:31:22 +0600

    event and functions

--- class2/main.js
+++ class2/main.js
@@ -79,3 +79,18 @@ function result_2(Do_1, Do_2) {
 result_2(50, 52);
 result_2(115, 80);
 result_2(25, 8); // the last function add html linkup 
+
+
+///Object 
+
+const daTa = {book:"500", pen:"Linkpro", clock:"Smart Watch", life:"Broken" };
+document.getElementById("demo").innerHTML = "This is Object output : " + daTa.book; // this line output show --> Output page 4th no line 
+
+document.write("This is object Result : " + daTa.clock); // this line output showing --> output page last line
+document.write(" <br>This is object Result : " + daTa.book);
+document.write(" <br>This is object Result : " + daTa.life);
+document.write(" <br>This is object Result : " + daTa.pen);
+
+
+
+///Event 
