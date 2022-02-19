d725e3c4354e232b241d33ef621ed64a139297f7
foysal <foysalf652@gmail.com>
2022-02-20 00:26:46 +0600

    Template Literals (ES6)

---
+++ Template_Literals.js
@@ -0,0 +1,14 @@
+//Template Literals (ES6) 
+// `` name backtick
+let data =`this is 
+Template Literals`;
+console.log(data)
+
+let age = 25;
+let name= "karimBro";
+let dob = "35 jun, 1859";
+
+console.log(`My name ${name}
+i am  ${age}
+My Date of Birth ${dob}`)
+
