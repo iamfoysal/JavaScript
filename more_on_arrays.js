b3b28ab767fbc0ad472866723c465bd4fc66f65d
foysal <foysalf652@gmail.com>
2022-02-20 00:06:49 +0600

    more on array

---
+++ more_on_arrays.js
@@ -0,0 +1,11 @@
+arr = [10,20,50,22]
+console.log(arr) //print arr
+console.log(arr[3])// print arr 3 no index
+// use arrays on more array
+
+arrs =[10,50,["Dhaka","City"],80,100,550]
+console.log(arrs)
+console.log(arrs[2])// now print ["Dhaka","City"], its 2 no index
+console.log(arrs[2][1]) // now i access in array 1 no index  
+arrss =[10,50,["Bangladesh",["Dhaka","City"],80,],100,550]
+console.log(arrss[2][1][0]) 
